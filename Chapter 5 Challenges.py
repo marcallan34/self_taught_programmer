47d4b303783e8e9018f8c094b71e5841d221bb2b
Marc Smith <marc.allan34@gmail.com>
2017-06-24 07:32:44 -0500

    Add files via upload

---
+++ Chapter 5 Challenges.py
@@ -0,0 +1,32 @@
+#Chapter five challenges
+
+#challenge 1
+fav_musicians = ['Bob Dylan', 'Todd Snider', 'Hayes Carll']
+
+
+#challenge 2
+places_lived = [(30.6280, 96.3344),(29.4241,98.4936)]
+
+
+#challenge 3
+self_info = {
+    'height': '5.11',
+    'favorite color': 'green',
+    'favorite movie': 'The Godfather'
+    }
+
+#challenge 4
+
+answer = input('Type: height, favorite color, or favorite movie     ')
+if answer in self_info:
+    print(self_info[answer])
+
+#Challenge 5
+
+fav_artists_songs = {
+    'Bob Dylan': ['Don\'t Think Twice It\'s Alright', 'Tangled up in Blue'],
+    'Todd Snider': ['The Devil you Know', 'Too Soon to Tell'],
+    'Hayes Carll': ['KMAG YOYO', 'Stom and Holler'],
+    }
+
+#Challenge 6
