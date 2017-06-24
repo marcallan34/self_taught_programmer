f3e0ee92cd03dcc5f32b75fdd4aadf743ab6e259
Marc Smith <marc.allan34@gmail.com>
2017-06-24 07:50:38 -0500

    Add files via upload

---
+++ Chapter 3 Challenges.py
@@ -0,0 +1,51 @@
+#Chapter 3 Challenges
+
+#Challenge 1
+print('string 1')
+print('string 2')
+print('last string')
+
+#Challenge 2
+number = 18
+
+if number < 10:
+    print('the variable \'number\' is less than ten')
+else:
+    print('the variable\'number\' is equal to or greater than 10')
+
+#Challenge 3
+
+second_number = 17
+
+if second_number <= 10:
+    print('The number is less than or equal to ten')
+elif 10 < second_number <=25:
+    print('The number is greater than 10, but not greater than 25')
+else:
+    print('The number is greater than 25')
+
+#Challenge 4
+
+first_number = 14
+second_number = 4
+
+print(first_number % second_number)
+
+#challenge 5
+
+first_number = 14
+second_number = 4
+
+print(first_number // second_number)
+
+
+#challenge 6
+
+age = 45
+
+if age < 30:
+    print('Wow, you are still young')
+elif 30 <= age <65:
+    print('#adulting')
+else:
+    print('Enjoy your remaining years!')
