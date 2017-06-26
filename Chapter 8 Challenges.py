8833c52edf4a42bac7fe6107320e104d46184872
Marc Smith <marc.allan34@gmail.com>
2017-06-25 21:12:25 -0500

    Update Chapter 8 Challenges.py

--- Chapter 8 Challenges.py
+++ Chapter 8 Challenges.py
@@ -6,7 +6,7 @@ low_median = statistics.median_low([1,3,5,7,9,11])
 print(low_median)
 
 #Challenge 2
-
+#see cubed.py within this repository to see the function cubed(x)
 import cubed
 
 cubed.cubed(4)
