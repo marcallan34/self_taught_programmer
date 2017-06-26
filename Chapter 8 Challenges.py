6817d70cd6bd04a64190881bd2294b8615ba7945
Marc Smith <marc.allan34@gmail.com>
2017-06-25 21:10:43 -0500

    Add files via upload

---
+++ Chapter 8 Challenges.py
@@ -0,0 +1,12 @@
+#Chapter 8 (modules) Challenges
+import statistics
+import math
+#Challenge 1
+low_median = statistics.median_low([1,3,5,7,9,11])
+print(low_median)
+
+#Challenge 2
+
+import cubed
+
+cubed.cubed(4)
---
+++ cubed.py
@@ -0,0 +1,5 @@
+import math
+
+def cubed(x):
+    x_cubed = math.pow(x,3)
+    print(x_cubed)
